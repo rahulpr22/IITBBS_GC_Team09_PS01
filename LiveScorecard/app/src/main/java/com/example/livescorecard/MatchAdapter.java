2731f0918be6285b04ce53bc7cb78fe769227969
Rahul <prb029@gmail.com>
2022-03-19 15:30:17 +0530

    Commit checkpoint1

--- LiveScorecard/app/src/main/java/com/example/livescorecard/MatchAdapter.java
+++ LiveScorecard/app/src/main/java/com/example/livescorecard/MatchAdapter.java
@@ -1,6 +1,7 @@
 package com.example.livescorecard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,34 +29,36 @@ public class MatchAdapter extends ArrayAdapter {
             // Layout Inflater inflates each item to be displayed in GridView.
             listitemView = LayoutInflater.from(getContext()).inflate(R.layout.listitem, parent, false);
         }
-        matchData product = (matchData) getItem(position);
+        matchData matchdata = (matchData) getItem(position);
         TextView team1 = listitemView.findViewById(R.id.Team1);
         TextView team2 = listitemView.findViewById(R.id.Team2);
         TextView t1Score= listitemView.findViewById(R.id.t1score);
         TextView t2Score= listitemView.findViewById(R.id.t2score);
         Button b= listitemView.findViewById(R.id.viewScore);
-        //TextView desc = listitemView.findViewById(R.id.prod_description);
-//        ImageView logo = listitemView.findViewById(R.id.prod_image);
-//        TextView view =listitemView.findViewById(R.id.viewbtn);
-//        title.setText(product.getTitle());
-//        price.setText(product.getPrice());
-//        //desc.setText(product.getDescription());
-//        //Log.d("test2","logo "+product.getLogo());
-//        Picasso.get().load(product.getLogo()).into(logo);
-//        view.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(mcontext,productScreen.class);
-//                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-//                i.putExtra("title",product.getTitle());
-//                i.putExtra("price",product.getPrice());
-//                i.putExtra("desc",product.getDescription());
-//                i.putExtra("logo",product.getLogo());
-//                i.putExtra("pid",product.getPid());
-//                i.putExtra("catid",product.getCatid());
-//                mcontext.startActivity(i);
-//            }
-//        });
+        team1.setText(matchdata.getTeam1());
+        team2.setText(matchdata.getTeam2());
+        t1Score.setText(matchdata.getTeam1Score());
+        t2Score.setText(matchdata.getTeam2Score());
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(mcontext,matchSummary.class);
+                i.putExtra("team1",matchdata.getTeam1());
+                i.putExtra("team2",matchdata.getTeam2());
+                i.putExtra("team1Score",matchdata.getTeam1Score());
+                i.putExtra("team2Score",matchdata.getTeam2Score());
+                i.putExtra("striker",matchdata.getStriker1());
+                i.putExtra("nonStriker",matchdata.getStriker2());
+                i.putExtra("striker1Runs",matchdata.getStriker1Runs());
+                i.putExtra("striker2Runs",matchdata.getStriker2Runs());
+                i.putExtra("bowler",matchdata.getBowler());
+                i.putExtra("bowlerStats",matchdata.getBowlerStats());
+                i.putExtra("team1Overs",matchdata.getTeam1OversPlayed());
+                i.putExtra("team2Overs",matchdata.getTeam2OversPlayed());
+                mcontext.startActivity(i);
+            }
+        });
+
 
         return listitemView;
     }
---
+++ LiveScorecard/app/src/main/java/com/example/livescorecard/matchSummary.java
@@ -0,0 +1,14 @@
+package com.example.livescorecard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class matchSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_summary);
+    }
+}
